cd014c4b8d62b4a447f5b8eb86d49dd918ffe9f1
utente <utente@Air-di-utente.lan>
2026-04-22 21:45:20 +0200

    fix: input_image corretto

--- api/genera.js
+++ api/genera.js
@@ -24,7 +24,7 @@ export default async function handler(req, res) {
       },
       body: JSON.stringify({
         input: {
-          image: imageBase64,
+          input_image: imageBase64,
           haircut_style: haircutStyle,
           hair_color: hairColor,
           gender: "female",
